8b61f94ec41218afb6b4a14369c95d5a8143b18f
remco@rem.co.com <remco@rem.co.com>
2023-02-28 21:42:34 -0600

    got some working now

--- regex.js
+++ regex.js
@@ -3,3 +3,7 @@ var emailRegex = /[0-9 -()+]+$/;
 var testr = /<(.+)>\s(.*)/;
 
 var test = /<(.+)>\s*(?:<!\[CDATA\[)?\s*(.*)\s*(?:]]>)?\s*<\/\1>/;
+
+var test2 = /<title>([aA-zZ 0-9/\-".]+)<\/title>\n?.*<link>([aA-zZ 0-9://.]+)<\/link>\n?.*/;
+
+var test2 = /<title>(.+)<\/title>\n.+<link>(.+)<\/link>\n.+\n.+\n.+\n.+<pubDate>(.+)<\/pubDate>\n.+\n.+<group>(.+)<\/group>/;
